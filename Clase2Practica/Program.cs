a677a12a3059549ce36a138553041ce5ec4e0b39
Tomas Seijas <tomas.seijas10@gmail.com>
2021-04-20 17:14:24 -0300

    Se agrega logica escribir -JSON

--- Clase2Practica/Program.cs
+++ Clase2Practica/Program.cs
@@ -10,9 +10,34 @@ namespace Clase2Practica
     {
         static void Main(string[] args)
         {
-            EjercicioLeerJson.Ejecutar();
+            EjercicioLeerJson.Ejecutar("ordenes");
+
+            EjercicioEscribirJson.Ejecutar(GenerarListaOrdenes(),"nuevasOrdenes");
+
         }
 
+        private static List<Orden> GenerarListaOrdenes()
+        {
+            Orden orden1 = new Orden();
+            Orden orden2 = new Orden();
 
+            orden1.nroOrden = 5;
+            orden2.nroOrden = 89;
+            orden1.descripcion = "vasos";
+            orden2.descripcion = "cubiertos";
+            orden1.cantidad = 2;
+            orden2.cantidad = 5;
+            orden1.precio = 500;
+            orden2.precio = 700;
+            orden1.entregada = false;
+            orden2.entregada = true;
+
+            List<Orden> ordenes = new List<Orden>();
+            ordenes.Add(orden1);
+            ordenes.Add(orden2);
+
+            return ordenes;
+
+        }
     }
 }
